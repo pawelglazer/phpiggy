84f52c1a2e91a7eb3e1a2cd33a2d13532ec97b28
Paweł Glazer <pawel.glazer@interia.pl>
2024-03-01 15:16:05 +0100

    Add Container class to Framework

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class Router 
+class Router
 {
     private array $routes = [];
+    private array $middleware = [];
 
-    public function add(string $method, string $path, array $controller) 
+    public function add(string $method, string $path, array $controller)
     {
         $this->routes[] = [
             'path' => $this->normalizePath($path),
@@ -17,7 +18,7 @@ class Router
         ];
     }
 
-    public function normalizePath(string $path):string
+    public function normalizePath(string $path): string
     {
         $path = trim($path, '/');
         $path = "/{$path}/";
@@ -26,22 +27,25 @@ class Router
         return $path;
     }
 
-    public function dispatch(string $path, string $method)
+    public function dispatch(string $path, string $method, Container $container = null)
     {
         $path = $this->normalizePath($path);
         $method = strtoupper($method);
 
-        foreach($this->routes as $route)
-        {
-            if(!preg_match("#^{$route['path']}$#",$path) || $route['method'] !== $method)
-            {
+        foreach ($this->routes as $route) {
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
                 continue;
             }
             [$class, $function] = $route['controller'];
 
-            $controllerInstance = new $class;
+            $controllerInstance = $container ? $container->resolve($class) : new $class;
 
             $controllerInstance->$function();
         }
+    }
+
+    public function addMiddleware(string $middleware)
+    {
+        $this->middleware[] = $middleware;
     }
 }
