21b631d5ed1c1b8919751cdb67e05a371a06b146
Paweł Glazer <pawel.glazer@interia.pl>
2024-03-02 14:05:31 +0100

    Add Middleware class to Framework

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -9,10 +9,12 @@ use App\Config\Paths;
 
 class HomeController
 {
-    
+
 
     public function __construct(private TemplateEngine $view)
     {
+        var_dump($this->view);
+        echo "<br>";
     }
 
     public function home()
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,7 +7,7 @@ namespace Framework;
 class Router
 {
     private array $routes = [];
-    private array $middleware = [];
+    private array $middlewares = [];
 
     public function add(string $method, string $path, array $controller)
     {
@@ -40,12 +40,21 @@ class Router
 
             $controllerInstance = $container ? $container->resolve($class) : new $class;
 
-            $controllerInstance->$function();
+            $action = fn () => $controllerInstance->{$function}();
+
+            foreach ($this->middlewares as $middleware) {
+                $middlewareInstance = $container ? $container->resolve($middleware) : new $middleware;
+                $action = fn () => $middlewareInstance->process($action);
+            }
+
+            $action();
+
+            return;
         }
     }
 
     public function addMiddleware(string $middleware)
     {
-        $this->middleware[] = $middleware;
+        $this->middlewares[] = $middleware;
     }
 }
